ef75359563a52a2de9bf4fc048add31eb047e304
mohammad Almamoon <almamoon604@gmail.com>
2022-11-09 20:52:27 +0600

    ADMIN LTE TEMPLATING DONE

---
+++ resources/views/backend/includes/script.blade.php
@@ -0,0 +1,22 @@
+<!-- jQuery -->
+<script src="{{ asset('backend/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap -->
+<script src="{{ asset('backend/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- overlayScrollbars -->
+<script src="{{ asset('backend/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('backend/dist/js/adminlte.js')}}"></script>
+
+<!-- OPTIONAL SCRIPTS -->
+<script src="{{asset('backend/dist/js/demo.js')}}"></script>
+
+<!-- jQuery Mapael -->
+<script src="{{ asset('backend/plugins/jquery-mousewheel/jquery.mousewheel.js')}}"></script>
+<script src="{{ asset('backend/plugins/raphael/raphael.min.js')}}"></script>
+<script src="{{ asset('backend/plugins/jquery-mapael/jquery.mapael.min.js')}}"></script>
+<script src="{{ asset('backend/plugins/jquery-mapael/maps/usa_states.min.js')}}"></script>
+<!-- ChartJS -->
+<script src="{{ asset('backend/plugins/chart.js/Chart.min.js')}}"></script>
+
+<!-- PAGE SCRIPTS -->
+<script src="{{asset('backend/dist/js/pages/dashboard2.js')}}"></script>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/dashboard',function(){
+    return view('backend.dashboard');
+})->name('dashboard');
+Route::get('/blank',function(){
+    return view('backend.blank');
+})->name('blank');
