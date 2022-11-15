9b157a049b91a868f8fe71e1a68a3eba2ecac987
marjanajenia <marjanajenia98@gmail.com>
2022-11-16 01:22:35 +0600

    UserProfile seeder add

---
+++ database/migrations/2022_11_15_191347_create_user_profiles_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profiles', function (Blueprint $table) {
+            $table->bigIncrements('profile_id');
+            $table->integer('user_id');
+            $table->string('full_name')->nullable();
+            $table->string('description')->nullable();
+            $table->string('facebook_link')->nullable();
+            $table->string('github_link')->nullable();
+            $table->string('profile_pic')->nullable();
+            $table->string('present_address')->nullable();
+            $table->string('parmenent_address')->nullable();
+            $table->integer('national_id')->nullable();
+            $table->string('balance')->default(0);
+            $table->string('user_skill')->nullable();
+            $table->string('user_position')->nullable();
+            $table->integer('online_status')->default(1);
+            $table->integer('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profiles');
+    }
+};
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\UserProfile;
 
 class UserSeeder extends Seeder
 {
@@ -18,12 +19,18 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-        User::create([
+        $user = User::insertGetId([
             'user_name' => 'admin',
             'role_id' => 1,
             'user_slug' => uniqid(),
             'email' => 'admin@mail.com',
             'password' => Hash::make('password'),
         ]);
+        UserProfile::create([
+            'user_id' => $user,
+            'full_name' => 'Admin',
+            'balance' => 0,
+        ]);
+
     }
 }
