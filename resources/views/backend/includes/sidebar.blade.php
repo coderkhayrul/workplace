0bfe0dac35ba57b5fc1d345ffb52139320d7b9f6
Khayrul Islam Shanto <77426516+coderkhayrul@users.noreply.github.com>
2022-12-16 01:38:58 +0600

    Sidebar Code Design Update

--- resources/views/backend/includes/sidebar.blade.php
+++ resources/views/backend/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-        <img src="{{asset('backend/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
+        <img src="{{ asset('backend/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">WorkPlace</span>
     </a>
@@ -11,10 +11,11 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-            <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('backend/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                    alt="User Image">
             </div>
             <div class="info">
-            <a href="#" class="d-block">{{ auth()->user()->user_name }}</a>
+                <a href="#" class="d-block">{{ auth()->user()->user_name }}</a>
             </div>
         </div>
         <!-- Sidebar Menu -->
@@ -24,7 +25,7 @@
                 <!-- Add icons to the links using the .nav-icon class
             with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{route('admin.dashboard')}}"
+                    <a href="{{ route('admin.dashboard') }}"
                         class="nav-link {{ Route::currentRouteName() == 'admin.dashboard' ? 'active' : '' }}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p> Dashboard </p>
@@ -33,8 +34,10 @@
 
                 <li class="nav-header">Management</li>
 
-                <li class="nav-item has-treeview {{ Route::is('admin.user.*') ? 'menu-open' : '' }} {{ Route::is('admin.role.*') ? 'menu-open' : '' }}">
-                    <a href="#" class="nav-link {{ Route::is('admin.user.*') ? 'active' : '' }}  {{ Route::is('admin.role.*') ? 'active' : '' }}">
+                <li
+                    class="nav-item has-treeview {{ Route::is('admin.user.*') ? 'menu-open' : '' }} {{ Route::is('admin.role.*') ? 'menu-open' : '' }}">
+                    <a href="#"
+                        class="nav-link {{ Route::is('admin.user.*') ? 'active' : '' }}  {{ Route::is('admin.role.*') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-users"></i>
                         <p>
                             User Manager
@@ -44,13 +47,15 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="{{ route('admin.user.index') }}" class="nav-link {{ Route::is('admin.user.*') ? 'active' : '' }}">
+                            <a href="{{ route('admin.user.index') }}"
+                                class="nav-link {{ Route::is('admin.user.*') ? 'active' : '' }}">
                                 <i class="fas fa-user-cog"></i>
                                 <p>User</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="{{ route('admin.role.index') }}" class="nav-link {{ Route::is('admin.role.*') ? 'active' : '' }}">
+                            <a href="{{ route('admin.role.index') }}"
+                                class="nav-link {{ Route::is('admin.role.*') ? 'active' : '' }}">
                                 <i class="fas fa-user-shield"></i>
                                 <p>Role</p>
                             </a>
@@ -60,16 +65,14 @@
                 <li class="nav-header">Short Link</li>
                 {{-- Live Visit --}}
                 <li class="nav-item">
-                    <a target="_blank" href="{{ route('web.home') }}"
-                        class="nav-link">
+                    <a target="_blank" href="{{ route('web.home') }}" class="nav-link">
                         <i class="nav-icon fas fa-globe text-primary"></i>
                         <p> Live Visit </p>
                     </a>
                 </li>
                 {{-- Logout --}}
                 <li class="nav-item">
-                    <a href="{{ route('admin.logout') }}"
-                        class="nav-link">
+                    <a href="{{ route('admin.logout') }}" class="nav-link">
                         <i class="nav-icon fas fa-sign-out-alt text-danger"></i>
                         <p> LOGOUT </p>
                     </a>
