0dfb3561137f6a3315f034f0c4db5a5aa9013650
Shomick_Hasan <shomick15-12158@diu.edu.bd>
2022-12-25 20:30:42 +0600

    service submition migrate

--- database/migrations/2022_12_25_142144_create_service_submites_table.php
+++ database/migrations/2022_12_25_142144_create_service_submites_table.php
@@ -15,6 +15,12 @@ return new class extends Migration
     {
         Schema::create('service_submites', function (Blueprint $table) {
             $table->id();
+            $table->integer('service_id');
+            $table->integer('buyer_id');
+            $table->integer('seller_id');
+            $table->string('submition_document')->nullable();
+            $table->string('submition_externalLink')->nullable();
+            $table->longText('submition_drescreption');
             $table->timestamps();
         });
     }
