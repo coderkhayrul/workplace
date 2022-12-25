543d1a9ea7562c83979603b2794204af60a7b059
Shomick_Hasan <shomick15-12158@diu.edu.bd>
2022-12-25 23:52:16 +0600

    service complete confermination complete

--- resources/views/backend/pages/service/Service_complete.blade.php
+++ resources/views/backend/pages/service/Service_complete.blade.php
@@ -31,13 +31,74 @@
                 </div>
             </div>
             <div class="card-body">
-                Start creating your amazing application!
+                <!--submition descreption-->
+                <div class="submition_des">
+                    <div class="row">
+                        <div class="colmd-6">
+                             <b><h5>Submition Descreption: &nbsp;</h5></b>
+                        </div>
+                        <div class="colmd-6">
+                           <p class="text-justify">{{$resive_submition->submition_drescreption}}</p>
+                        </div>
+                    </div>
+                </div><!--end submition descreption-->
+                <!--submition file-->
+                <hr>
+                <div class="submition_file">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <b><h5>Submition Document:&nbsp;</h5></b>
+                        </div>
+                        <div class="col-md-6">
+                            @if($resive_submition->submition_document)
+                                <a href="{{route('buyer.servicecomplete.download',$resive_submition->submition_document)}}" class="text-justify">Download</a>
+                            @else
+                                <span class="text-justify">Not available</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="external-link">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <b><h5>External link:&nbsp;</h5></b>
+                        </div>
+                        <div class="col-md-6">
+                            @if($resive_submition->submition_externalLink)
+                                <a href="{{$resive_submition->submition_externalLink}}" target="_blank" class="text-justify">{{$resive_submition->submition_externalLink}}</a>
+                            @else
+                                <span class="text-justify">Not available</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="card-footer">
-                Footer
+                @if($resive_submition->PlaceBit->status==1)
+                    <a href="{{route('buyer.bid.completejob',$resive_submition->PlaceBit->id)}}" class="btn btn-outline-primary">Job Complete</a>
+                @elseif($resive_submition->PlaceBit->status==2)
+                    <span class="badge bg-success">Successfully Complete Job</span>
+                @endif
             </div>
         </div>
     @else
+        <div class="card">
+            <div class="card-header">
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                Seller Not submited...
+            </div>
+            <div class="card-footer">
+            Thank you
+            </div>
+        </div>
 
     @endif
 
